cb09234488f859f81dfe41f8cd9b94c0807d585f
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2023-01-23 11:47:22 +0700

    new

--- git-commands.js
+++ git-commands.js
@@ -4,3 +4,13 @@
 // git log
 // git remote add origin https://github.com/12rgilang/Agile-SCM.git (warning: first initiate only)
 // git push origin master (untuk mengepush ke branch master)
+
+// untuk menambahkan di branch dan menambahkan fitur baru
+// git checkout -b Day01_FiturVariable
+// git add .
+// git commit -m ""
+// git push origin nama branch
+
+// untuk pindah ke branch lain
+// git checkout master (untuk masuk ke suatu branch)
+// git pull origin master (untuk mendapatkan update dari suatu branch)
