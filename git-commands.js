a503a53e20777a4bff32b64ec368635dff8ab659
Gilang Ramadhan <gilang.ramadhan011297+github@gmail.com>
2023-01-23 11:12:10 +0700

    Updates

---
+++ git-commands.js
@@ -0,0 +1,6 @@
+// git init (inside project directory) (first initiate)
+// git add (file name) atau git add .
+// git commit -m "Update variable"
+// git log
+// git remote add origin https://github.com/12rgilang/Agile-SCM.git (warning: first initiate only)
+// git push origin master (untuk mengepush ke branch master)
